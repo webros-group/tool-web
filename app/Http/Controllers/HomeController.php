050b05cf5d0d3b600c235849644009dd807bc7d4
NganTLT <ngantlt@vitalify.asia>
2019-01-29 14:38:40 +0700

    seconte commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,9 +28,6 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-
-        //paypal
-        
         return view('home');
     }
 
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -10,15 +9,41 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <!-- Fonts -->
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ asset('fashe/images/icons/favicon.png') }}"/>
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/fonts/themify/themify-icons.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/fonts/elegant-font/html-css/style.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/animsition/css/animsition.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/daterangepicker/daterangepicker.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/slick/slick.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/vendor/lightbox2/css/lightbox.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/css/util.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('fashe/css/main.css') }}">
+    <!--===============================================================================================-->
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
     
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet" type="text/css">
+    <!-- <link href="{{ asset('css/responsive.css') }}" rel="stylesheet" type="text/css"> -->
       
